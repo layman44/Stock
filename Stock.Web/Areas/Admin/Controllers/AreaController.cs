fc71107392c7e52323eb95357b7238a3405a9724
layman44 <yuchao44@qq.com>
2016-09-05 00:35:36 +0800

    修改AreaController
    修改AreaController

--- Stock.Web/Areas/Admin/Controllers/AreaController.cs
+++ Stock.Web/Areas/Admin/Controllers/AreaController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Stock.Area;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,23 @@ namespace Stock.Web.Areas.Admin.Controllers
 {
     public class AreaController : Controller
     {
+        private readonly IAreaService _areaService;
+        public AreaController(IAreaService areaService)
+        {
+            _areaService = areaService;
+        }
+
         // GET: Admin/Area
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetRoots()
+        {
+            var output = await _areaService.GetRoots();
+            return Json(output, JsonRequestBehavior.AllowGet);
         }
     }
 }
